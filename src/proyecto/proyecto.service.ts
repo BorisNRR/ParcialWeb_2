995f24541c8727c5089f4fca80de3f5b3749b53e
BorisNRR <b.reyesr@uniandes.edu.co>
2024-05-23 10:49:38 -0500

    service proyecto

--- src/proyecto/proyecto.service.ts
+++ src/proyecto/proyecto.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { ProyectoEntity } from './proyecto.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class ProyectoService {}
+export class ProyectoService {
+
+    constructor(
+        @InjectRepository(ProyectoEntity)
+        private readonly propuestaRepository: Repository<ProyectoEntity>
+    ){}
+
+    async createPropuesta(proyecto: ProyectoEntity): Promise<ProyectoEntity>{
+        if(proyecto.fecha_fin > proyecto.fecha_inicio){
+        }
+        return await this.propuestaRepository.save(proyecto)
+    }
+}
