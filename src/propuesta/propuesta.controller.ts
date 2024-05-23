629b0faf1905cd8a7ef22deb04eadd0ddd3c5f59
BorisNRR <b.reyesr@uniandes.edu.co>
2024-05-23 17:44:16 -0500

    Controller method findAll

--- src/propuesta/propuesta.controller.ts
+++ src/propuesta/propuesta.controller.ts
@@ -1,9 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseInterceptors, Get } from '@nestjs/common';
 import { PropuestaService } from './propuesta.service';
+import { BusinessErrorsInterceptor } from 'src/shared/interceptors/business-errors/business-errors.interceptor';
 
 @Controller('propuesta')
+@UseInterceptors(BusinessErrorsInterceptor)
 export class PropuestaController {
     constructor( private readonly propuestaService: PropuestaService ){}
 
+    @Get()
+    async findAll(){
+        return await this.propuestaService.findAll()
+    }
+
     
+
+
+
 }
